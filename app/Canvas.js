ec70381b01bbcdaf429411b87bedd84854f4b4e1
Ishaan Jain <ishaanj1@gmail.com>
2023-06-22 15:49:35 -0700

    no ssr, committing out folder with index.html

--- app/Canvas.js
+++ app/Canvas.js
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { useRef, useEffect, useState } from "react";
 import { eigs, norm, transpose } from 'mathjs'
 
--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
+'use client'
+
 import styles from './page.module.css';
 import dynamic from 'next/dynamic'
+
+import Canvas from './Canvas';
 
 const Home = dynamic(() => import('./Canvas'), {
   ssr: false,
